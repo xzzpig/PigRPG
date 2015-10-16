d4a712f3de714c824b79a4a45d5acb2a4d1c809e
xzzpig <1146628972@qq.com>
2015-10-16 16:24:57 +0800

    First PUSH

---
+++ src/com/github/xzzpig/pigrpg/Main.java
@@ -0,0 +1,26 @@
+package com.github.xzzpig.pigrpg;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.xzzpig.pigrpg.commands.Commands;
+
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnable() {
+	getLogger().info(getName()+"插件已被加载");
+	saveDefaultConfig();
+	}
+	
+	//插件停用函数
+	@Override
+	public void onDisable() {
+	getLogger().info(getName()+"插件已被停用 ");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender,Command cmd,String label,String[] args)  {
+		return Commands.command(sender, cmd, label, args);
+	}
+}
---
+++ src/com/github/xzzpig/pigrpg/commands/Commands.java
@@ -0,0 +1,10 @@
+package com.github.xzzpig.pigrpg.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class Commands {
+	public static boolean command(CommandSender sender,Command cmd,String label,String[] args)  {
+		return false;
+	}
+}
