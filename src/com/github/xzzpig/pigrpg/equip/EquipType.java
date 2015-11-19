c944cf528c33d7107ca2840e87ac7cef4155a4ed
xzzpig <1146628972@qq.com>
2015-11-19 23:17:53 +0800

    更新装备

--- src/com/github/xzzpig/pigrpg/equip/EquipType.java
+++ src/com/github/xzzpig/pigrpg/equip/EquipType.java
@@ -2,17 +2,21 @@ package com.github.xzzpig.pigrpg.equip;
 import java.util.*;
 
 public enum EquipType{
-	Weapon("武器",false),Core("核心"),Head("头盔"),Chest("护甲"),Leg("战靴"),Hand("首饰"),Neck("项链"),Consume("消耗品");
+	Default("无",false),Weapon("武器",false),Core("核心"),Head("头盔"),Chest("护甲"),Leg("战靴"),Hand("首饰"),Neck("项链"),Consume("消耗品");
 	
 	private static HashMap<String,EquipType> typelist = new HashMap<String,EquipType>();
 	
 	private String typename;
 	private boolean show = true;
 	
-	public EquipType getFrom(String typename){
+	public static EquipType getFrom(String typename){
 		if(typelist.containsKey(typename))
 			return typelist.get(typename);
 		return null;
+	}
+	
+	public static boolean hasType(String typename){
+		return typelist.containsKey(typename);
 	}
 	
 	EquipType(String typename){
--- src/com/github/xzzpig/pigrpg/equip/Equipment.java
+++ src/com/github/xzzpig/pigrpg/equip/Equipment.java
@@ -1,5 +1,8 @@
 package com.github.xzzpig.pigrpg.equip;
+import java.util.*;
+import org.bukkit.*;
 import org.bukkit.inventory.*;
+import org.bukkit.inventory.meta.*;
 
 public class Equipment extends ItemStack
 {
@@ -8,11 +11,35 @@ public class Equipment extends ItemStack
 	public Equipment(int id)
 	{
 		super(id);
+		if(id == 0)
+			super.setTypeId(1);
+		this.loadEnums();
+	}
+	public Equipment(ItemStack is)
+	{
+		super(is);
+		if(this.getType() == Material.AIR)
+			super.setTypeId(1);
+		loadEnums();
 	}
 	
 	public Equipment setEquiptype(EquipType etype)
 	{
 		this.etype = etype;
+		if(this.hasLoreType()){
+			ItemMeta im = this.getItemMeta();
+			List<String> lore = im.getLore();
+			lore.set(0,"        "+etype.toString());
+			im.setLore(lore);
+			this.setItemMeta(im);
+		}
+		else{
+			ItemMeta im = this.getItemMeta();
+			List<String> lore = im.getLore();
+			lore.add(0,"        "+etype.toString());
+			im.setLore(lore);
+			this.setItemMeta(im);
+		}
 		return this;
 	}
 	public EquipType getEquiptype()
@@ -20,5 +47,25 @@ public class Equipment extends ItemStack
 		return etype;
 	}
 	
+	public boolean hasLoreType(){
+		ItemMeta im = this.getItemMeta();
+		List<String> lore = im.getLore();
+		if(lore == null)
+			return false;
+		if(EquipType.hasType(lore.get(0).replaceAll(" ","")))
+			return true;
+		return false;
+	}
 	
+	public void loadEnums(){
+		ItemMeta im = this.getItemMeta();
+		List<String> lore = im.getLore();
+		if(lore == null){
+			return;
+		}
+		if(this.hasLoreType())
+			this.setEquiptype(EquipType.getFrom(lore.get(0).replaceAll(" ","")));
+		else
+			this.setEquiptype(EquipType.Default);
+	}
 }
